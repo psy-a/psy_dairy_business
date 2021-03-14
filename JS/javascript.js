11228cc5bba582a7d913d3933b69f9bff416ff67
psy-a <psyfi488@gmail.com>
2021-03-14 20:54:43 +0300

    Add JavaScript function for daily milk production reporting.

---
+++ JS/javascript.js
@@ -0,0 +1,19 @@
+var m1 = 510;
+var m2 = 308;
+var m3 = 486;
+var m4 = 572;
+var productionShedA = function () {
+    alert("Your Milk Production for Today in Shed A is "+ m1 +" Litres.");
+};
+var productionShedB = function () {
+    alert("Your Milk Production for Today in Shed B is "+ m2 +" Litres.");
+};
+var productionShedC = function () {
+    alert("Your Milk Production for Today in Shed C is "+ m3 +" Litres.");
+};
+var productionShedD = function () {
+    alert("Your Milk Production for Today in Shed D is "+ m4 +" Litres.");
+};
+var totalProductionToday = function () {
+    alert("Your Total Milk Production for Today is "+ (m1+m2+m3+m4) +" Litres.");
+}
